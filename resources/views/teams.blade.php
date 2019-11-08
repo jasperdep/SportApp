87653402c91f1becc199c264aad55e2980dfdf6e
Jasper De Pooter <jappie1999@gmail.com>
2019-11-08 09:34:41 +0100

    logo bij teams

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Liverpool</title>
+    <title>{{ $liverpool['name'] }}</title>
 </head>
 <body>
     <div class="container">
-    <h1>{{ $liverpool['name'] }}</h1>
+    <h1>{{ $liverpool['name'] }} <img src="{{ $liverpool['crestUrl'] }}" alt="profile Pic" height="60" max-width="100%"></h1>
     <table class="table">
         <thead>
             <tr>
